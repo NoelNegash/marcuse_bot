62d4e38c3bb873cb43e290a30d54a21afe234c1e
NoelNegash <noelnegash@gmail.com>
2023-06-16 08:17:19 +0300

    data

--- api/webhook.js
+++ api/webhook.js
@@ -8,7 +8,7 @@ async function listMembers(db, bot, chatId) {
   members.forEach((m) => {
     m = m.data()
 
-    message += `${m.name}` + (m.isAdmin ? ' [ADMIN]' : '');
+    message += `${m.name} ${m.isAdmin ? ' [ADMIN]' : ''} | ${m.borrowed_books.length} books currently borrowed.`;
   })
   await bot.sendMessage(chatId, message, {parse_mode: 'html'});
 }
