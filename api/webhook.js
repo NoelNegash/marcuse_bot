821a5aff814d00c875f7dad9395716360e3d9ba5
NoelNegash <noelnegash@gmail.com>
2023-07-16 11:00:39 +0300

    fix

--- api/webhook.js
+++ api/webhook.js
@@ -7,7 +7,16 @@ function prettifyBook(book, verbose = false) {
   return res
 }
 function quoteSplit(s) {
-  return s.match(/(?:[^\s"]+|"[^"]*")+/g) 
+  return str.match(/\\?.|^$/g).reduce((p, c) => {
+      if(c === '"'){
+          p.quote ^= 1;
+      }else if(!p.quote && c === ' '){
+          p.a.push('');
+      }else{
+          p.a[p.a.length-1] += c.replace(/\\(.)/,"$1");
+      }
+      return  p;
+  }, {a: ['']}).a
 }
 
 async function listMembers(db, bot, chatId) {
